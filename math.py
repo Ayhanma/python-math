230de91a29f37691f75b0a07fc0fcee26ddfed9c
ayhanma <ayhanorazov2008@gmail.com>
2024-01-09 16:57:08 +0500

    git commit mathAdd

---
+++ math.py
@@ -0,0 +1,17 @@
+import math
+x = -3.4
+print(math.ceil(x))
+# Округление до ближайшего большего числа
+print(math.fabs(x))
+# Выводит модуль число
+x1 = 18
+print(math.factorial(x1))
+# Выводит факториал числа
+print(math.pi)
+# Выводит число Пи
+print(math.e)
+# выводит число e
+print(math.fmod(x1, x))
+# остаток от деления x1 на x
+print(math.modf(x1))
+# Возвращает дробную и целую часть числа X. Оба числа имеют тот же знак, что и X.
